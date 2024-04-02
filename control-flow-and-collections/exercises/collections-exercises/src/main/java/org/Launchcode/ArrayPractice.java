d322b08298f30ea31ce928de47704e241c34ad5a
Matt Stephens <mdstephens33@hotmail.com>
2024-04-02 16:18:37 -0500

    exercises

---
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/Launchcode/ArrayPractice.java
@@ -0,0 +1,17 @@
+package org.Launchcode;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+        for (double i : integerArray) {
+            System.out.println(i);
+        }
+        String phrase = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+        String[] words = phrase.split(" ");
+        System.out.println(Arrays.toString(words));
+        String[] sentences = phrase.split("\\.");
+        System.out.println(Arrays.toString(sentences));
+    }
+}
