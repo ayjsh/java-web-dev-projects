f1b1473ce6b03d7eb906504082ac749bb8150dec
Matt Stephens <mdstephens33@hotmail.com>
2024-04-01 20:31:44 -0500

    Bonus 2

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -9,13 +9,15 @@ public class Area {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius:");
-
-        if (input.hasNextDouble() && input.nextDouble() > 0) {
-            radius = input.nextDouble();
-            double area = Circle.getArea(radius);
-            System.out.println("The area of a circle of radius " + radius + " is " + area);
-        } else {
-            System.out.println("error, that's not a valid radius");
+        while (input.nextDouble() < 0) {
+            System.out.println("Enter a radius:");
+            if (input.hasNextDouble()) {
+                radius = input.nextDouble();
+                double area = Circle.getArea(radius);
+                System.out.println("The area of a circle of radius " + radius + " is " + area);
+            } else {
+                System.out.println("error, that's not a valid radius");
+            }
         }
         input.close();
 
