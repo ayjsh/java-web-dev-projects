2b6ae5208fa2fbe0801f70b9cefc00aaa7eea0b0
Matt Stephens <mdstephens33@hotmail.com>
2024-04-04 20:42:48 -0500

    Studio

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaRectangle.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import java.util.Scanner;
+public class AreaRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, can I have your rectangle's length?");
+
+        int length = input.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
