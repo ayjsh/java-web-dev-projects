4137688b86ec4904b049c9ea26ec46620671874e
Matt Stephens <mdstephens33@hotmail.com>
2024-04-29 14:47:32 -0500

    Exceptions Exercises

---
+++ inheritance/testing-inheritance/exercises/src/Test/java/CatTest.java
@@ -0,0 +1,8 @@
+
+class CatTest {
+    @Test
+    public void inheritsSuperInFirstConstructor() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+        assertEquals(7, keyboardCat.getWeight(), .001);
+    }
+}
