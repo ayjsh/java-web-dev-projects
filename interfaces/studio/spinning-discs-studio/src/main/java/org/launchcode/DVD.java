45f275bc5b12bc21576ee44d3d5e5cf16aac43a5
Matt Stephens <mdstephens33@hotmail.com>
2024-04-29 20:22:41 -0500

    Studio debugging

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/DVD.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/DVD.java
@@ -20,6 +20,7 @@ public class DVD extends BaseDisc implements OpticalDisc{
     }
 
 
+
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
 
--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/OpticalDisc.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/OpticalDisc.java
@@ -5,4 +5,7 @@ public interface OpticalDisc {
 
 
 
+
+
+
 }
